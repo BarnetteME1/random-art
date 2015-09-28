46477ce3c53244b9f5f83609405c02b9304f4f2f
BarnetteME1 <BarnetteME1@gmail.com>
2015-09-28 19:19:02 -0400

    random_art.py

---
+++ functions to put in.py
@@ -0,0 +1,15 @@
+def blue_duck():
+"lambda x, y: abs(" + random.choice(math_sin) + random.choice(num_list) + random.choice(math_op) + random.choice(X_Y) + "))"
+s = "lambda x, y: x * y" + random.choice(["+", "-", "*"]) + "abs(y) ** abs(x)"
+t = "lambda x, y: abs(" + random.choice(["math.cos(", "math.sin(", "math.tan("]) + "x)" + random.choice(["+", "-", "*"]) + random.choice(["(math.cos(", "(math.sin(", "(math.tan("]) + "y)))"
+u = "lambda x, y: abs(" + random.choice(["math.cos(", "math.sin(", "math.tan("]) + random.choice(["x", "y"]) + random.choice(["+", "-", "*"]) + random.choice(["x)", "y)"]) + "**" + "abs(" + random.choice(["x", "y"]) +"))"
+v = "lambda x, y: abs(abs(x) ** abs(math.sin(random.random())) + (abs(y) ** abs(math.sin(x * random.random()))))"
+w = "lambda x, y: abs(math.cos(x * math.sin(y * math.tan(x * math.pi + math.cos(random.random())))))"
+x = "lambda x, y: abs(math.cos(math.sin(math.tan(x + y))))"
+y = "lambda x, y: abs(abs(x + y * math.cos(x + y)) ** abs(math.sin(x + y)))"
+z = "lambda x, y: abs((abs(x) ** abs(y + math.tan(x))) * math.sin(abs(x) ** abs(math.cos(y))))"
+  return eval(random.choice([a, b, c, d, e, f, g, h]))
+
+random.choice(["math.cos(", "math.sin(", "math.tan("])
+random.choice(["+", "-", "*"])
+random.choice(["x", "y"])
--- random_art.py
+++ random_art.py
@@ -1,14 +1,33 @@
 import random
+import math
 
 # Your job is to create better version of create_expression and
 # run_expression to create random art.
 # Your expression should have a __str__() function defined for it.
 
+num_list = ["42.94011220646039", "5.646665186128161", "36.181321872403686",
+"73.588459320803684", "47.46616486634289", "9.28960463563688", "92.13071170982583",
+"67.2557323771716", "32.13986413171649", "23.053905506440437"]
+math_sin = ["math.cos(", "math.sin(", "math.tan("]
+math_op = ["+", "-", "*"]
+x_y = ["x", "y"]
+pi_e = ["math.pi", "math.e"]
+def sven():
+    z = "lambda x, y:" + random.choice(math_sin) + random.choice(pi_e) + random.choice(math_op) + random.choice(math_sin) + random.choice(pi_e) + random.choice(math_op) + random.choice(math_sin) + random.choice(pi_e) + random.choice(math_op) + random.choice(math_sin) + random.choice(pi_e) + random.choice(math_op) + random.choice(math_sin) + random.choice(pi_e) + random.choice(math_op) + random.choice(x_y) + ")" + random.choice(math_op) + random.choice(x_y) + "))" + random.choice(math_op) + random.choice(math_sin) + random.choice(pi_e) + random.choice(math_op) + random.choice(x_y) + ")))"
+
+    return eval(z)
+
+def ramona():
+    w = "lambda x, y: abs(" + random.choice(math_sin) + random.choice(x_y) + random.choice(math_op) + random.choice(num_list) + ")" + random.choice(math_op) + "abs(" + random.choice(x_y) + ")" + "** abs(" + random.choice(x_y) + "))"
+    x = "lambda x, y: abs(" + random.choice(math_sin) + random.choice(x_y) + random.choice(math_op) + "(" + random.choice(math_sin) + random.choice(num_list) + "))))"
+    y = "lambda x, y: abs(" + random.choice(math_sin) + random.choice(x_y) + random.choice(math_op) + random.choice(x_y) + "**" + "abs(" + random.choice(num_list) +")))"
+    z = "lambda x, y: abs(" + random.choice(math_sin) + random.choice(num_list) + random.choice(math_op) + random.choice(x_y) + "))"
+    return eval(sven())
 
 def create_expression():
     """This function takes no arguments and returns an expression that
     generates a number between -1.0 and 1.0, given x and y coordinates."""
-    expr = lambda x, y: (random.random() * 2) - 1
+    expr = sven()
     return expr
 
 
